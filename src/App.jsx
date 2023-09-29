81860233e5506480f8b97a35ece637e56913cbb6
ZSR <2472990740@qq.com>
2023-09-29 20:45:58 +0800

    使用三维视角

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import { Viewer, Ion } from 'cesium';
+import {
+	Viewer,
+	Ion,
+	Terrain,
+	Cartesian3,
+	Math as CesiumMath,
+	createOsmBuildingsAsync,
+} from 'cesium';
 import { useEffect } from 'react';
 
 function App() {
 	useEffect(() => {
 		Ion.defaultAccessToken =
 			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkMDI4ODUwYS1hYzg5LTQ0ZWEtOWZmMC04ZjNlOWI4NjlkYjAiLCJpZCI6MTY4Mzk0LCJpYXQiOjE2OTU2MzU4NDh9.SNGjXAB_YsCAvH0afN2RmR6Dem_UD5rpIyP4x15rZOY';
-		const viewer = new Viewer('cesiumApp');
+		const viewer = new Viewer('cesiumApp', {
+			terrain: Terrain.fromWorldTerrain(),
+		});
+
+		viewer.camera.flyTo({
+			destination: Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+			orientation: {
+				heading: CesiumMath.toRadians(0, 0),
+				pitch: CesiumMath.toRadians(-15.0),
+			},
+		});
+
+		(async () => {
+			const buildingTileset = await createOsmBuildingsAsync();
+			viewer.scene.primitives.add(buildingTileset);
+		})();
 
 		return () => viewer.destroy();
 	}, []);
