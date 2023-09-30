8c521ed5521ac15a94fda99a313667d63ddad493
ZSR <2472990740@qq.com>
2023-09-30 20:19:04 +0800

    实现了3D模型加载

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,81 @@
 import {
 	Viewer,
 	Ion,
+	Terrain,
 	Cartesian3,
 	createOsmBuildingsAsync,
-	Color,
-	JulianDate,
-	SampledPositionProperty,
-	TimeIntervalCollection,
-	TimeInterval,
-	PathGraphics,
 	IonResource,
-	VelocityOrientationProperty,
-	Terrain,
+	GeoJsonDataSource,
+	ClassificationType,
+	Cesium3DTileStyle,
+	Cesium3DTileset,
 } from 'cesium';
 
-import flightData from './assets/flightData.js';
 import { useEffect } from 'react';
 
 const accessToken =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkMDI4ODUwYS1hYzg5LTQ0ZWEtOWZmMC04ZjNlOWI4NjlkYjAiLCJpZCI6MTY4Mzk0LCJpYXQiOjE2OTU2MzU4NDh9.SNGjXAB_YsCAvH0afN2RmR6Dem_UD5rpIyP4x15rZOY';
 
 function App() {
-	/**
-	 * @param {Viewer} viewer
-	 */
-	async function loadOSMBuildingsAysnc(viewer) {
-		const osmBuildings = await createOsmBuildingsAsync();
-		viewer.scene.primitives.add(osmBuildings);
-	}
-
 	useEffect(() => {
 		const viewer = new Viewer('cesiumApp', { terrain: Terrain.fromWorldTerrain(), infoBox: false });
 
 		Ion.defaultAccessToken = accessToken;
 
-		loadOSMBuildingsAysnc(viewer);
+		viewer.camera.flyTo({
+			destination: Cartesian3.fromDegrees(-104.9965, 39.74248, 4000),
+		});
 
-		const timeStepInSeconds = 30;
-		const totalSeconds = timeStepInSeconds * (flightData.length - 1);
-		const start = JulianDate.fromIso8601('2020-03-09T23:10:00Z');
-		const stop = JulianDate.addSeconds(start, totalSeconds, new JulianDate());
-		viewer.clock.startTime = start.clone();
-		viewer.clock.stopTime = stop.clone();
-		viewer.clock.currentTime = start.clone();
-		viewer.timeline.zoomTo(start, stop);
-		// Speed up the playback speed 50x.
-		viewer.clock.multiplier = 50;
-		// Start playing the scene.
-		viewer.clock.shouldAnimate = true;
-
-		const positionProperty = new SampledPositionProperty();
-
-		for (let i = 0; i < flightData.length; i++) {
-			const dataPoint = flightData[i];
-
-			// Declare the time for this individual sample and store it in a new JulianDate instance.
-			const time = JulianDate.addSeconds(start, i * timeStepInSeconds, new JulianDate());
-			const position = Cartesian3.fromDegrees(
-				dataPoint.longitude,
-				dataPoint.latitude,
-				dataPoint.height
-			);
-			// Store the position along with its timestamp.
-			// Here we add the positions all upfront, but these can be added at run-time as samples are received from a server.
-			positionProperty.addSample(time, position);
-
-			viewer.entities.add({
-				description: `Location: (${dataPoint.longitude}, ${dataPoint.latitude}, ${dataPoint.height})`,
-				position: position,
-				point: { pixelSize: 10, color: Color.RED },
-			});
+		async function addBuildingGeoJSON() {
+			// Load the GeoJSON file from Cesium ion.
+			const geoJSONURL = await IonResource.fromAssetId(2295981);
+			// Create the geometry from the GeoJSON, and clamp it to the ground.
+			const geoJSON = await GeoJsonDataSource.load(geoJSONURL, { clampToGround: true });
+			// Add it to the scene.
+			const dataSource = await viewer.dataSources.add(geoJSON);
+			// By default, polygons in CesiumJS will be draped over all 3D content in the scene.
+			// Modify the polygons so that this draping only applies to the terrain, not 3D buildings.
+			for (const entity of dataSource.entities.values) {
+				entity.polygon.classificationType = ClassificationType.TERRAIN;
+			}
+			// Move the camera so that the polygon is in view.
+			viewer.flyTo(dataSource);
 		}
 
-		async function loadModel() {
-			// Load the glTF model from Cesium ion.
-			const airplaneUri = await IonResource.fromAssetId(2295911);
-			const airplaneEntity = viewer.entities.add({
-				availability: new TimeIntervalCollection([new TimeInterval({ start: start, stop: stop })]),
-				position: positionProperty,
-				// Attach the 3D model instead of the green point.
-				model: { uri: airplaneUri },
-				// Automatically compute the orientation from the position.
-				orientation: new VelocityOrientationProperty(positionProperty),
-				path: new PathGraphics({ width: 3 }),
+		(async () => {
+			const buildingsTileset = await createOsmBuildingsAsync();
+			viewer.scene.primitives.add(buildingsTileset);
+
+			await addBuildingGeoJSON();
+
+			buildingsTileset.style = new Cesium3DTileStyle({
+				show: {
+					conditions: [
+						// Any building that has this elementId will have `show = false`.
+						['${elementId} === 332469316', false],
+						['${elementId} === 332469317', false],
+						['${elementId} === 235368665', false],
+						['${elementId} === 530288180', false],
+						['${elementId} === 530288179', false],
+						// If a building does not have one of these elementIds, set `show = true`.
+						[true, true],
+					],
+				},
+
+				// Set the default color style for this particular 3D Tileset.
+				// For any building that has a `cesium#color` property, use that color, otherwise make it white.
+				color:
+					"Boolean(${feature['cesium#color']}) ? color(${feature['cesium#color']}) : color('#ffffff')",
 			});
 
-			viewer.trackedEntity = airplaneEntity;
-		}
+			// STEP 6 CODE
+			// Add the 3D Tileset you created from your Cesium ion account.
+			const newBuildingTileset = await Cesium3DTileset.fromIonAssetId(2296027);
+			viewer.scene.primitives.add(newBuildingTileset);
 
-		loadModel();
+			// Move the camera to the new building.
+			viewer.flyTo(newBuildingTileset);
+		})();
 
 		return () => viewer.destroy();
 	}, []);
@@ -96,6 +83,7 @@ function App() {
 	return (
 		<>
 			<div id="cesiumApp"></div>
+			<button onClick={() => []}>切换</button>
 		</>
 	);
 }
